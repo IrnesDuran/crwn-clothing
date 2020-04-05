3896c3814b4e1b118ed7ee1d811cc35bed637c88
darkhitect1985 <55331764+darkhitect1985@users.noreply.github.com>
2020-04-05 13:46:44 +0200

    header basic setup

--- src/App.js
+++ src/App.js
@@ -4,14 +4,16 @@ import './App.css';
 
 import HomePage from './pages/homepage/homepage.component';
 import ShopPage from './pages/shop/shop.component';
+import Header from './components/header/header.component';
 
 
 function App() {
   return (
     <div>
+      <Header/>
       <Switch>
         <Route exact path='/' component= {HomePage}/>
-        <Route path='/hats' component= {ShopPage}/>
+        <Route path='/shop' component= {ShopPage}/>
       </Switch>
     </div>
   );
