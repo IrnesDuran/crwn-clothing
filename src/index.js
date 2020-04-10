25068b993d26961affcfc1cc8445365b4c6f2855
darkhitect1985 <55331764+darkhitect1985@users.noreply.github.com>
2020-04-10 17:04:54 +0200

    redux store, reducer and actions

--- src/index.js
+++ src/index.js
@@ -3,11 +3,15 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import { BrowserRouter } from 'react-router-dom';
 import App from './App';
+import { Provider } from 'react-redux';
+import store from './redux/store'
 
 
 ReactDOM.render(
-    <BrowserRouter>
-        <App />
-    </BrowserRouter>
+    <Provider store={store}> 
+        <BrowserRouter>
+            <App />
+        </BrowserRouter>
+    </Provider>
 , document.getElementById('root'));
 
