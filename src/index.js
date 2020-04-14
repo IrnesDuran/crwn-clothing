3e82c73c77009978c359a1cdd1cfb59d959708c0
darkhitect1985 <55331764+darkhitect1985@users.noreply.github.com>
2020-04-14 12:44:19 +0200

    localStorage for cart using Redux Persist

--- src/index.js
+++ src/index.js
@@ -4,13 +4,17 @@ import './index.css';
 import { BrowserRouter } from 'react-router-dom';
 import App from './App';
 import { Provider } from 'react-redux';
-import store from './redux/store'
+import {store, persistor} from './redux/store';
+import {PersistGate} from 'redux-persist/integration/react';
+
 
 
 ReactDOM.render(
     <Provider store={store}> 
         <BrowserRouter>
-            <App />
+            <PersistGate persistor={persistor}>
+                <App />
+            </PersistGate>
         </BrowserRouter>
     </Provider>
 , document.getElementById('root'));
--- src/redux/root-reducer.js
+++ src/redux/root-reducer.js
@@ -4,7 +4,18 @@ import userReducer from '../redux/user/user.reducer';
 
 import cartReducer from '../redux/cart/cart.reducer'
 
-export default combineReducers({
+import { persistReducer } from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
+
+const persistConfig = {
+    key: 'root',
+    storage,
+    whitelist: ['cart']
+};
+
+const rootReducer = combineReducers({
     user: userReducer,
     cart: cartReducer
 });
+
+export default persistReducer(persistConfig, rootReducer);
