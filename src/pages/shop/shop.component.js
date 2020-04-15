c7e87216974976eec5d33bf3bf589052029e87d5
darkhitect1985 <55331764+darkhitect1985@users.noreply.github.com>
2020-04-15 15:00:57 +0200

    collections separated through route params

--- src/pages/shop/shop.component.js
+++ src/pages/shop/shop.component.js
@@ -1,11 +1,15 @@
+  
 import React from 'react';
+import { Route } from 'react-router-dom';
 
-import CollectionOverview from '../../components/collection-overview/collection-overview.component';
+import CollectionsOverview from '../../components/collection-overview/collection-overview.component';
+import CollectionPage from '../collection/collection.component';
 
-const ShopPage = (props) => (
-      <div className='shop-page'>
-          <CollectionOverview/>
-      </div>
+const ShopPage = ({ match }) => (
+  <div className='shop-page'>
+    <Route exact path={`${match.path}`} component={CollectionsOverview} />
+    <Route path={`${match.path}/:collectionId`} component={CollectionPage} />
+  </div>
 );
 
 export default ShopPage;
