fec204cab931fc671f42808d633bd57aafbe7a18
darkhitect1985 <55331764+darkhitect1985@users.noreply.github.com>
2020-04-16 11:14:05 +0200

    update of shop data and reducers

--- src/components/collection-overview/collection-overview.component.js
+++ src/components/collection-overview/collection-overview.component.js
@@ -1,26 +1,23 @@
 import React from 'react';
+import { connect } from 'react-redux';
+import { createStructuredSelector } from 'reselect';
+
+import CollectionPreview from '../collection-preview/collection-preview.component';
+
+import { selectCollectionsForPreview } from '../../redux/shop/shop.selectors';
+
 import './collection-overview.styles.scss';
-import {connect} from 'react-redux';
-// import {createStructuredSelector} from 'reselect';
 
-import CollectionPreview from '../../components/collection-preview/collection-preview.component';
-
-const CollectionOverview =({collections}) => (
-    <div className='collections-overview'>
-        {
-        collections.map(({ id, ...otherCollectionProps }) => (
-          <CollectionPreview key={id} {...otherCollectionProps} />))
-        }
-    </div>
+const CollectionsOverview = ({ collections }) => (
+  <div className='collections-overview'>
+    {collections.map(({ id, ...otherCollectionProps }) => (
+      <CollectionPreview key={id} {...otherCollectionProps} />
+    ))}
+  </div>
 );
 
-// const mapStateToProps = createStructuredSelector({
-//     collection: selectCollections
-//   });
-
-const mapStateToProps = state => ({
-    collections: state.shop.collections
+const mapStateToProps = createStructuredSelector({
+  collections: selectCollectionsForPreview
 });
-  
-  export default connect(mapStateToProps)(CollectionOverview);
-  
+
+export default connect(mapStateToProps)(CollectionsOverview);
--- src/redux/directory/directory.reducer.js
+++ src/redux/directory/directory.reducer.js
@@ -10,27 +10,27 @@ const INITIAL_STATE = {
           title: 'jackets',
           imageUrl: 'https://i.ibb.co/px2tCc3/jackets.png',
           id: 2,
-          linkUrl: ''
+          linkUrl: 'shop/jackets'
         },
         {
           title: 'sneakers',
           imageUrl: 'https://i.ibb.co/0jqHpnp/sneakers.png',
           id: 3,
-          linkUrl: ''
+          linkUrl: 'shop/sneakers'
         },
         {
           title: 'womens',
           imageUrl: 'https://i.ibb.co/GCCdy8t/womens.png',
           size: 'large',
           id: 4,
-          linkUrl: ''
+          linkUrl: 'shop/womens'
         },
         {
           title: 'mens',
           imageUrl: 'https://i.ibb.co/R70vBrQ/men.png',
           size: 'large',
           id: 5,
-          linkUrl: ''
+          linkUrl: 'shop/mens'
         }
       ]
 };
